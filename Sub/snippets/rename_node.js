5dccb22166b0bc0d21b9a99ae45fd662208866bc
Harrison <Harrison@fanruan.com>
2024-08-19 20:18:10 +0800

    合并订阅时，支持对节点的分组，从而屏蔽不支持 SS 的节点

--- Sub/snippets/rename_node.js
+++ Sub/snippets/rename_node.js
@@ -1,10 +1,10 @@
 function rename(node) {
-     const host = node.Hostname;
-     if (host.contains("jiedian")) {
-        return node.Remark + "_Cheap";
-     }
-     if (host.contains("incomparablebeauty")) {
-        return node.Remark + "_Candy";
-     }
-    return node.Remark;
+   const host = node.Hostname;
+   if (host.includes("jiedian")) {
+      return node.Remark + "_Cheap";
+   }
+   if (host.includes("incomparablebeauty")) {
+      return node.Remark + "_Candy";
+   }
+  return node.Remark;
 }
